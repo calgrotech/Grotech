1563330841fe69e91fa8da379ddfd654bfa247d0
Yujie Wang <yujiewang@berkeley.edu>
2019-10-28 02:25:23 -0700

    refactored code, added dynamic savefiles

--- raspi/measure.py
+++ raspi/measure.py
@@ -1,62 +1,74 @@
 import os
-import time
-import serial
+import re
+import yaml
+from serial import Serial
 from time import sleep
 from datetime import datetime
 from subprocess import call
-import re
-import json
+from itertools import count
+
+# may add argparse
+## required
+num_data = 3
+## optional
+sleep_sec = 60
+baudrate = 115200
+serial_port = path.join(os.sep, 'dev'. 'ttyACM0')
+save_delta = 5
 
 time_file_name = datetime.now().strftime('-%d-%m-%Y')
-curr_dir = os.getcwd()
-save_folder = os.path.join(curr_dir, "data")
-save_place = os.path.join(save_folder, "temperature_dump{0}.json".format(time_file_name))
-json_file = open(save_place, "a+")
+save_folder = os.path.join(os.getcwd(), "data")
 
-ser = serial.Serial('/dev/ttyACM0', 115200) #Arduino Serial Port
-
-#ser.write(b'8')
-#while not ser.in_waiting:
-#    ser.write(b'8')
+ser = Serial(serial_port, baudrate)
 
 class ArdString:
-    def __init__(self, ard_string):
+    def __init__(self, ard_string, num_data, start_char = '[', end_char = ']'):
         self.ard_string = str(ard_string)
+        self.start_char, self.end_char = start_char, end_char
+        self.num_data = num_data
     
     @property
-    def valid_pack(self, required_chars = ['[', ']'], num_data = 3):
-        for char in required_chars:
-            if char not in self.ard_string:
-                return False
-        arduino_ints = self.get_arduino_ints(start_char = required_chars[0], end_char = required_chars[-1])
-        if len(arduino_ints) != num_data:
-            print(arduino_ints)
+    def has_start_end(self):
+        if self.start_char not in self.ard_string or self.end_char not in self.ard_string:
+            return False
+        return True
+
+    @property
+    def is_valid_pack(self):
+        if not self.has_start_end:
+            return False
+        if len(self.data_list) != self.num_data:
             return False
         return True
     
-    def get_arduino_ints(self, start_char = '[', end_char = ']'):
-        after_start = self.ard_string.split(start_char)[1]
-        before_end = after_start.split(end_char)[0]
-        int_list = [int(s) for s in re.findall("\\d+", before_end)]
-        return int_list
+    @property
+    def data_list(self):
+        if not self.has_start_end:
+            return []
+        between_chars = self.ard_string.split(self.start_char)[1].split(self.end_char)[0]
+        return [int(s) for s in re.findall("\\d+", between_chars)]
 
-def get_time_dict(temp_list):
-    temp_list = ard_obj.get_arduino_ints()
-    temp_dict = {'S1': temp_list[0], 'S2': temp_list[1], 'S3': temp_list[2]}
-    return {current_time.strftime('%m/%d/%Y/%H:%M:%S'): temp_dict}
+    @property
+    def time_dict(self):
+        data_dict = {'S{0}'.format(sensor_no): self.data_list[sensor_no] for sensor_no in range(len(self.data_list))}
+        return {datetime.now().strftime('%m/%d/%Y/%H:%M:%S'): data_dict}
+
+
+get_save_place = lambda: os.path.join(save_folder, "temperature_dump{0}.yaml".format(time_file_name))
 
 #Loop through sensor data
-while True:
-    current_time = datetime.now() #Get Time
+for s in count(0):
     input_arduino = ser.readline() #Read Serial line
     ard_obj = ArdString(input_arduino)
-    if ard_obj.valid_pack:
-        json_file = open(save_place, "a+")
-        temp_list = ard_obj.get_arduino_ints()
-        time_dict = get_time_dict(temp_list)
-        print(time_dict)
-        time_json = json.dumps(time_dict, indent = 4)
-        json_file.write(time_json + "\n\n")
-        json_file.close()
+
+    with open(get_save_place(), "a+") as yaml_file:
+        if not ard_obj.is_valid_pack:
+            continue
+        print(ard_obj.time_dict)
+        time_yaml = yaml.dumps(ard_obj.time_dict, indent = 4)
+        yaml_file.write(time_yaml + "\n\n")
+
+    if s % save_delta == 0:
         call(["rclone", "copy", save_folder, "grotech:"])
-    time.sleep(60) #grab data every min
+
+    sleep(sleep_sec)
