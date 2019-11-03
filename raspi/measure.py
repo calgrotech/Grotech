db0e492af55a8814b4f4fd4f506f548f0453d31c
Yujie Wang <yujiewang@berkeley.edu>
2019-11-02 23:47:26 -0700

    bugfixes

--- raspi/measure.py
+++ raspi/measure.py
@@ -13,11 +13,11 @@ num_data = 3
 ## optional
 sleep_sec = 60
 baudrate = 115200
-serial_port = path.join(os.sep, 'dev'. 'ttyACM0')
+serial_port = os.path.join(os.sep, 'dev', 'ttyACM0')
 save_delta = 5
 
-time_file_name = datetime.now().strftime('-%d-%m-%Y')
-save_folder = os.path.join(os.getcwd(), "data")
+time_file_name = lambda: datetime.now().strftime('-%d-%m-%Y')
+save_folder = os.path.join(os.path.dirname(os.getcwd()), "data")
 
 ser = Serial(serial_port, baudrate)
 
@@ -54,18 +54,18 @@ class ArdString:
         return {datetime.now().strftime('%m/%d/%Y/%H:%M:%S'): data_dict}
 
 
-get_save_place = lambda: os.path.join(save_folder, "temperature_dump{0}.yaml".format(time_file_name))
+get_save_place = lambda: os.path.join(save_folder, "temperature_dump{0}.yaml".format(time_file_name()))
 
-#Loop through sensor data
-for s in count(0):
+#Loop through sensor data (from -1 since the first packet is usually invalid)
+for s in count(-1):
     input_arduino = ser.readline() #Read Serial line
-    ard_obj = ArdString(input_arduino)
+    ard_obj = ArdString(input_arduino, num_data)
 
     with open(get_save_place(), "a+") as yaml_file:
         if not ard_obj.is_valid_pack:
             continue
         print(ard_obj.time_dict)
-        time_yaml = yaml.dumps(ard_obj.time_dict, indent = 4)
+        time_yaml = yaml.dump(ard_obj.time_dict, indent = 4)
         yaml_file.write(time_yaml + "\n\n")
 
     if s % save_delta == 0:
